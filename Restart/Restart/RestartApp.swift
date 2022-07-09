08336da75a6ee85d247a0bec539d9434e46cd3b8
zelihauslu <zelusluz@gmail.com>
2022-07-09 16:11:26 +0300

    Restart

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Zeliha Uslu on 9.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
