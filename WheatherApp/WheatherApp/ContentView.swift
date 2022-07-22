8cc4a0b7f4fdf55c45e476c9f976718cd808963b
zelihauslu <zelusluz@gmail.com>
2022-07-22 17:00:56 +0300

    first commit

---
+++ WheatherApp/WheatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WheatherApp
+//
+//  Created by Zeliha Uslu on 22.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WheatherApp/WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Zeliha Uslu on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
